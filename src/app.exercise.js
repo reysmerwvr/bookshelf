b8c2f491fe77c8343908ef6da97a6f6f8e5a0d8a
Reysmer Valle <reysmerwvr@gmail.com>
2023-09-24 17:20:10 +0200

    add user data on page load

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -3,14 +3,31 @@ import {jsx} from '@emotion/core'
 
 import * as React from 'react'
 import * as auth from 'auth-provider'
+import {client} from './utils/api-client'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
 
 function App() {
   const [user, setUser] = React.useState(null)
 
-  const login = form => auth.login(form).then(user => setUser(user))
-  const register = form => auth.register(form).then(user => setUser(user))
+  React.useEffect(() => {
+    getUser().then(u => setUser(u))
+  }, [])
+
+  const login = form => auth.login(form).then(u => setUser(u))
+  const register = form => auth.register(form).then(u => setUser(u))
   const logout = () => {
     auth.logout()
     setUser(null)
@@ -24,8 +41,3 @@ function App() {
 }
 
 export {App}
-
-/*
-eslint
-  no-unused-vars: "off",
-*/
